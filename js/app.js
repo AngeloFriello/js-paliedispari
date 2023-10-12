2856ba5cd5e2bbe8e406a61a4930bc230692f8ad
angelo friello <angelofriello01@gmail.com>
2023-10-12 15:08:52 +0200

    add split word

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 // - Chiedere all’utente di inserire una parola 
 //     - crea un prompt per chiedere la parola
-const wordUtent = prompt('inserisci una parola') 
+const wordUtent = prompt('inserisci una parola')
+console.log(wordUtent)
 // - Creare una funzione per capire se la parola inserita è palindroma
 //     - creare una funzione
+
 //     - trattare la stringa presa dal prompt come un array
-//     - inserire la condizione che se la stringa invertita è uguale a la stringa scritta dall'utente la risposta e true
+//     - inserire la condizione che s
+//e la stringa invertita è uguale a la stringa scritta dall'utente la risposta e true
 //     - inserire l'altra conidzione per cui e false 
+
+
+
+
+
+function reverseString(wordUtent) {
+    // Usa il metodo split per restituire un nuovo array
+    let splitString = wordUtent.split("");
+    console.log(splitString)
+    // Usa il metodo reverse per invertire l'array appena creato
+    let reverseArray = splitString.reverse();
+    console.log(reverseArray)
+    // Usa il metodo join per unire tutti gli elementi della stringa in un array
+    let joinArray = reverseArray.join(""); 
+    console.log(joinArray)
+    //Step 4. Restituisci la stringa invertita
+    return joinArray;
+}
+ 
+reverseString(wordUtent);
